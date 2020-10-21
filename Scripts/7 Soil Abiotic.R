2af304ddd3ec6a1378ef474e7b0d4cf09898a3ee
paulgdennis <p.dennis@uq.edu.au>
2020-10-21 12:47:53 +1000

    add letters to posthoc

--- Scripts/7 Soil Abiotic.R
+++ Scripts/7 Soil Abiotic.R
@@ -8,7 +8,10 @@ for(i in colnames(SoilChem[,56:93])){
   print(anova(lm(SoilChem[,i]~SoilChem$Rate)))
 }
 
-
+for(i in colnames(SoilChem[,56:93])) {
+  print(i)
+  print(cld(lsmeans(aov(SoilChem[,i] ~ factor(Rate), data= SoilChem), ~ factor(Rate), Letters = letters, adjust = "Tukey")))
+}
 
 
 

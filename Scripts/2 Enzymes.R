627cec8d1ff75d6e75ddfc728f23a4b13bfccf43
paulgdennis <p.dennis@uq.edu.au>
2020-10-21 12:41:57 +1000

    Added letters for posthocs

--- Scripts/2 Enzymes.R
+++ Scripts/2 Enzymes.R
@@ -1,17 +1,22 @@
 ### Enzymes
 
 source('Functions/Functions.R')
+library(multcomp)
 
 for(i in colnames(enzymes)) {
   print(i)
   print(summary(aov(enzymes[,i] ~ data.all$Rate)))
   }
 
+#for(i in colnames(enzymes)) {
+#  print(i)
+#  print(TukeyHSD(aov(enzymes[,i] ~ factor(data.all$Rate))))
+#}
+
 for(i in colnames(enzymes)) {
   print(i)
-  print(TukeyHSD(aov(enzymes[,i] ~ factor(data.all$Rate))))
+  print(cld(lsmeans(aov(enzymes[,i] ~ factor(Rate), data= data.all), ~ factor(Rate), Letters = letters, adjust = "Tukey")))
 }
-
 
 library(sciplot)
 svg("C:/Users/cfors/Dropbox/Alpha/Field experiment/R/N-Trial/Plots/Enzme_barplots.svg")

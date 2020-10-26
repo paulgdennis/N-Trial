9f865ae1353f4bd9c7d9d598abefb2e735b0a1bb
s4605964 <alpha_gulanes@yahoo.com>
2020-10-27 08:19:16 +1000

    Abiotic bar graphs of ANOVA

--- Scripts/7 Soil Abiotic.R
+++ Scripts/7 Soil Abiotic.R
@@ -3,17 +3,86 @@ SoilChem <- read.table("Data/N_trial_data.csv", header = TRUE, sep=',')
 
 names(SoilChem)
 
-for(i in colnames(SoilChem[,56:93])){
+for(i in colnames(SoilChem[,56:92])){
   print(i)
   print(anova(lm(SoilChem[,i]~SoilChem$Rate)))
 }
 
-for(i in colnames(SoilChem[,56:93])) {
+library(multcomp) 
+
+for(i in colnames(SoilChem[,56:92])) {
   print(i)
   print(cld(lsmeans(aov(SoilChem[,i] ~ factor(Rate), data= SoilChem), ~ factor(Rate), Letters = letters, adjust = "Tukey")))
 }
 
 
+library(sciplot)
 
+# significant means (decreasing with N dose)
+bargraph.CI(SoilChem$Rate, SoilChem$EC,main="Electrical Conductivity")
+bargraph.CI(SoilChem$Rate, SoilChem$pH.cacl2,main="pH CaCl2")
+bargraph.CI(SoilChem$Rate, SoilChem$pH.h2o,main="pH Water")
+bargraph.CI(SoilChem$Rate, SoilChem$CEC,main="CEC")
+bargraph.CI(SoilChem$Rate, SoilChem$S,main="Sulphur")
+bargraph.CI(SoilChem$Rate, SoilChem$Ca.amm.ace,main="Calcium (Amm.acet.)Meq.100g")
+
+
+
+
+
+# significant but needs to check further with Tukey (also with the other significant paramenters)
+bargraph.CI(SoilChem$Rate, SoilChem$Grass.index,main="Grass index")
+
+
+
+
+
+# significant means (increasing with N dose)
+bargraph.CI(SoilChem$Rate, SoilChem$Al.ppm,main="Aluminum KCL (mg/kg)")
+bargraph.CI(SoilChem$Rate, SoilChem$Al.KCl,main="Al KCl (Meq.100g")
+bargraph.CI(SoilChem$Rate, SoilChem$Al.sat,main="Al % Saturattion")
+bargraph.CI(SoilChem$Rate, SoilChem$Fe,main="Iron")
+
+
+bargraph.CI(SoilChem$Rate, SoilChem$K.avail,main="Available Potassium")
+bargraph.CI(SoilChem$Rate, SoilChem$K.amm.ace,main="Potassium (Amm.acet.)Meq.100g")
+
+bargraph.CI(SoilChem$Rate, SoilChem$Mg.amm.ace,main="Mg (Amm.acet.) Meq.100g")
+bargraph.CI(SoilChem$Rate, SoilChem$Mg.amm.ace.1,main="Mg (Amm.acet.) %")
+
+
+
+
+
+
+# verify nitrogen level
+bargraph.CI(SoilChem$Rate, SoilChem$NO3.N,main="Nitrate Nitrogen")
+
+
+
+
+
+
+
+# not significant means with increasing N dose)
+bargraph.CI(SoilChem$Rate, SoilChem$CN,main="C:N Ratio")
+bargraph.CI(SoilChem$Rate, SoilChem$ESP,main="Sodium % of Cations")
+bargraph.CI(SoilChem$Rate, SoilChem$OM,main="Organic Matter (%) ")
+bargraph.CI(SoilChem$Rate, SoilChem$OrgC,main="Organic Carbon (%)")
+bargraph.CI(SoilChem$Rate, SoilChem$TC,main="Total Carbon (combustion)(%)")
+bargraph.CI(SoilChem$Rate, SoilChem$TN,main="Total Nitrogen (Combustion)(%)")
+bargraph.CI(SoilChem$Rate, SoilChem$Cl,main="Chloridev (mg.kg)")
+
+
+bargraph.CI(SoilChem$Rate, SoilChem$PBI,main="Phosphorus Buffer Index")
+bargraph.CI(SoilChem$Rate, SoilChem$P.colwell,main="Phosphorus")
+
+bargraph.CI(SoilChem$Rate, SoilChem$Cu,main="Copper")
+bargraph.CI(SoilChem$Rate, SoilChem$Mn,main="Manganese")
+bargraph.CI(SoilChem$Rate, SoilChem$Zn,main="Zinc")
+
+bargraph.CI(SoilChem$Rate, SoilChem$Clay.pc,main="Clay %")
+bargraph.CI(SoilChem$Rate, SoilChem$Sand.pc,main="Sand %")
+bargraph.CI(SoilChem$Rate, SoilChem$Silt.pc,main="Silt %")
 
 

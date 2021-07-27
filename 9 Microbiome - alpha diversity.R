460b75cd75b37c88b6121863e95485570eb7335d
Microbellology <annabelle.clarke@uqconnect.edu.au>
2021-07-27 16:14:21 +1000

    Added saving

--- 9 Microbiome - alpha diversity.R
+++ 9 Microbiome - alpha diversity.R
@@ -17,17 +17,22 @@ for(i in colnames(env.16S.all[,5:9])) {
   print(summary(aov(env.16S.all[,i] ~ env.16S.all$Compartment * factor(env.16S.all$Rate))))
 }
 
+##Soil
 
 for(i in colnames(env.16S.soil[,5:9])) {
   print(i)
   print(summary(aov(env.16S.soil[,i] ~ factor(env.16S.soil$Rate))))
 }
 
+
 bargraph.CI(env.16S.soil$Rate, env.16S.soil$Sobs)
 bargraph.CI(env.16S.soil$Rate, env.16S.soil$Chao1)
 bargraph.CI(env.16S.soil$Rate, env.16S.soil$PD)
 bargraph.CI(env.16S.soil$Rate, env.16S.soil$Shan)
 bargraph.CI(env.16S.soil$Rate, env.16S.soil$Simp)
+
+
+##Ecto
 
 for(i in colnames(env.16S.ecto[,5:9])) {
   print(i)
@@ -39,6 +44,8 @@ bargraph.CI(env.16S.ecto$Rate, env.16S.ecto$Chao1)
 bargraph.CI(env.16S.ecto$Rate, env.16S.ecto$PD)
 bargraph.CI(env.16S.ecto$Rate, env.16S.ecto$Shan)
 bargraph.CI(env.16S.ecto$Rate, env.16S.ecto$Simp)
+
+##Endo
 
 for(i in colnames(env.16S.endo[,5:9])) {
   print(i)
@@ -75,6 +82,8 @@ for(i in colnames(env.ITS.all[,5:8])) {
   print(cld(lsmeans(aov(env.ITS.all[,i] ~ factor(Rate), data= env.ITS.all), ~ factor(Rate), Letters = letters, adjust = "Tukey")))
 }
 
+##Soil
+
 for(i in colnames(env.ITS.soil[,5:8])) {
   print(i)
   print(summary(aov(env.ITS.soil[,i] ~ factor(env.ITS.soil$Rate))))
@@ -84,6 +93,8 @@ bargraph.CI(env.ITS.soil$Rate, env.ITS.soil$Sobs)
 bargraph.CI(env.ITS.soil$Rate, env.ITS.soil$Chao1)
 bargraph.CI(env.ITS.soil$Rate, env.ITS.soil$Shan)
 bargraph.CI(env.ITS.soil$Rate, env.ITS.soil$Simp)
+
+##Ecto
 
 for(i in colnames(env.ITS.ecto[,5:8])) {
   print(i)
@@ -95,6 +106,8 @@ bargraph.CI(env.ITS.ecto$Rate, env.ITS.ecto$Chao1)
 bargraph.CI(env.ITS.ecto$Rate, env.ITS.ecto$Shan)
 bargraph.CI(env.ITS.ecto$Rate, env.ITS.ecto$Simp)
 
+#Endo
+
 for(i in colnames(env.ITS.endo[,5:8])) {
   print(i)
   print(summary(aov(env.ITS.endo[,i] ~ factor(env.ITS.endo$Rate))))
@@ -104,3 +117,158 @@ bargraph.CI(env.ITS.endo$Rate, env.ITS.endo$Sobs)
 bargraph.CI(env.ITS.endo$Rate, env.ITS.endo$Chao1)
 bargraph.CI(env.ITS.endo$Rate, env.ITS.endo$Shan)
 bargraph.CI(env.ITS.endo$Rate, env.ITS.endo$Simp)
+
+
+##Code for saving (as above but with extra steps)##
+
+# 16S
+
+##Soil
+
+svg("../Data/Figures/16S.soil.sobs.svg")
+bargraph.CI(env.16S.soil$Rate, env.16S.soil$Sobs)
+dev.off()
+cld(lsmeans(aov(env.16S.soil$Sobs ~ factor(Rate), data= env.16S.soil), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.soil.chao1.svg")
+bargraph.CI(env.16S.soil$Rate, env.16S.soil$Chao1)
+dev.off()
+cld(lsmeans(aov(env.16S.soil$Chao1 ~ factor(Rate), data= env.16S.soil), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.soil.pd.svg")
+bargraph.CI(env.16S.soil$Rate, env.16S.soil$PD)
+dev.off()
+cld(lsmeans(aov(env.16S.soil$PD ~ factor(Rate), data= env.16S.soil), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.soil.shan.svg")
+bargraph.CI(env.16S.soil$Rate, env.16S.soil$Shan)
+dev.off()
+cld(lsmeans(aov(env.16S.soil$Shan ~ factor(Rate), data= env.16S.soil), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.soil.simp.svg")
+bargraph.CI(env.16S.soil$Rate, env.16S.soil$Simp)
+dev.off()
+cld(lsmeans(aov(env.16S.soil$Simp ~ factor(Rate), data= env.16S.soil), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+##Ecto
+
+svg("../Data/Figures/16S.ecto.sobs.svg")
+bargraph.CI(env.16S.ecto$Rate, env.16S.ecto$Sobs)
+dev.off()
+cld(lsmeans(aov(env.16S.ecto$Sobs ~ factor(Rate), data= env.16S.ecto), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.ecto.chao1.svg")
+bargraph.CI(env.16S.ecto$Rate, env.16S.ecto$Chao1)
+dev.off()
+cld(lsmeans(aov(env.16S.ecto$Chao1 ~ factor(Rate), data= env.16S.ecto), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.ecto.pd.svg")
+bargraph.CI(env.16S.ecto$Rate, env.16S.ecto$PD)
+dev.off()
+cld(lsmeans(aov(env.16S.ecto$PD ~ factor(Rate), data= env.16S.ecto), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.ecto.shan.svg")
+bargraph.CI(env.16S.ecto$Rate, env.16S.ecto$Shan)
+dev.off()
+cld(lsmeans(aov(env.16S.ecto$Shan ~ factor(Rate), data= env.16S.ecto), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.ecto.simp.svg")
+bargraph.CI(env.16S.ecto$Rate, env.16S.ecto$Simp)
+dev.off()
+cld(lsmeans(aov(env.16S.ecto$Simp ~ factor(Rate), data= env.16S.ecto), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+##Endo
+
+svg("../Data/Figures/16S.endo.sobs.svg")
+bargraph.CI(env.16S.endo$Rate, env.16S.endo$Sobs)
+dev.off()
+cld(lsmeans(aov(env.16S.endo$Sobs ~ factor(Rate), data= env.16S.endo), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.endo.chao1.svg")
+bargraph.CI(env.16S.endo$Rate, env.16S.endo$Chao1)
+dev.off()
+cld(lsmeans(aov(env.16S.endo$Chao1 ~ factor(Rate), data= env.16S.endo), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.endo.pd.svg")
+bargraph.CI(env.16S.endo$Rate, env.16S.endo$PD)
+dev.off()
+cld(lsmeans(aov(env.16S.endo$PD ~ factor(Rate), data= env.16S.endo), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.endo.shan.svg")
+bargraph.CI(env.16S.endo$Rate, env.16S.endo$Shan)
+dev.off()
+cld(lsmeans(aov(env.16S.endo$Shan ~ factor(Rate), data= env.16S.endo), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/16S.endo.simp.svg")
+bargraph.CI(env.16S.endo$Rate, env.16S.endo$Simp)
+dev.off()
+cld(lsmeans(aov(env.16S.endo$Simp ~ factor(Rate), data= env.16S.endo), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+# ITS
+
+##Soil
+
+svg("../Data/Figures/ITS.soil.sobs.svg")
+bargraph.CI(env.ITS.soil$Rate, env.ITS.soil$Sobs)
+dev.off()
+cld(lsmeans(aov(env.ITS.soil$Sobs ~ factor(Rate), data= env.ITS.soil), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/ITS.soil.chao1.svg")
+bargraph.CI(env.ITS.soil$Rate, env.ITS.soil$Chao1)
+dev.off()
+cld(lsmeans(aov(env.ITS.soil$Chao1 ~ factor(Rate), data= env.ITS.soil), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/ITS.soil.shan.svg")
+bargraph.CI(env.ITS.soil$Rate, env.ITS.soil$Shan)
+dev.off()
+cld(lsmeans(aov(env.ITS.soil$Shan ~ factor(Rate), data= env.ITS.soil), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/ITS.soil.simp.svg")
+bargraph.CI(env.ITS.soil$Rate, env.ITS.soil$Simp)
+dev.off()
+cld(lsmeans(aov(env.ITS.soil$Simp ~ factor(Rate), data= env.ITS.soil), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+##Ecto
+
+svg("../Data/Figures/ITS.ecto.sobs.svg")
+bargraph.CI(env.ITS.ecto$Rate, env.ITS.ecto$Sobs)
+dev.off()
+cld(lsmeans(aov(env.ITS.ecto$Sobs ~ factor(Rate), data= env.ITS.ecto), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/ITS.ecto.chao1.svg")
+bargraph.CI(env.ITS.ecto$Rate, env.ITS.ecto$Chao1)
+dev.off()
+cld(lsmeans(aov(env.ITS.ecto$Chao1 ~ factor(Rate), data= env.ITS.ecto), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/ITS.ecto.shan.svg")
+bargraph.CI(env.ITS.ecto$Rate, env.ITS.ecto$Shan)
+dev.off()
+cld(lsmeans(aov(env.ITS.ecto$Shan ~ factor(Rate), data= env.ITS.ecto), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/ITS.ecto.simp.svg")
+bargraph.CI(env.ITS.ecto$Rate, env.ITS.ecto$Simp)
+dev.off()
+cld(lsmeans(aov(env.ITS.ecto$Simp ~ factor(Rate), data= env.ITS.ecto), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+##Endo
+
+svg("../Data/Figures/ITS.endo.sobs.svg")
+bargraph.CI(env.ITS.endo$Rate, env.ITS.endo$Sobs)
+dev.off()
+cld(lsmeans(aov(env.ITS.endo$Sobs ~ factor(Rate), data= env.ITS.endo), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/ITS.endo.chao1.svg")
+bargraph.CI(env.ITS.endo$Rate, env.ITS.endo$Chao1)
+dev.off()
+cld(lsmeans(aov(env.ITS.endo$Chao1 ~ factor(Rate), data= env.ITS.endo), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/ITS.endo.shan.svg")
+bargraph.CI(env.ITS.endo$Rate, env.ITS.endo$Shan)
+dev.off()
+cld(lsmeans(aov(env.ITS.endo$Shan ~ factor(Rate), data= env.ITS.endo), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
+svg("../Data/Figures/ITS.endo.simp.svg")
+bargraph.CI(env.ITS.endo$Rate, env.ITS.endo$Simp)
+dev.off()
+cld(lsmeans(aov(env.ITS.endo$Simp ~ factor(Rate), data= env.ITS.endo), ~ factor(Rate), Letters = letters, adjust = "Tukey"))
+
